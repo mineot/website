e7f02435b8aa38ae84c6f3d81913e8d0eb62c71b
Mineot <mineot@gmail.com>
2023-04-07 23:24:24 -0300

    alpha/styleconf - create conf styles

--- src/style/index.ts
+++ src/style/index.ts
@@ -3,27 +3,16 @@ import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
-const websiteTheme = {
-  dark: true,
-  colors: {
-    background: "#000", // black
-    surface: "#222", // light black
-    primary: "#CDDC39", // lime
-    secondary: "#BA68C8", // purple lighten-2
-    error: "#E53935", // red darken-1
-    info: "#2196F3", // blue
-    success: "#009688", // teal
-    warning: "#ff6f00", // amber darken-4
-  },
-};
+let styleConf = await fetch("./conf/style.json");
+let theme = await styleConf.json();
+
+if (Object.keys(theme).length === 0) {
+  styleConf = await fetch("./conf/style.default.json");
+  theme = await styleConf.json();
+}
 
 const style = createVuetify({
-  theme: {
-    defaultTheme: "websiteTheme",
-    themes: {
-      websiteTheme,
-    },
-  },
+  theme,
   icons: {
     defaultSet: "mdi",
     aliases,
