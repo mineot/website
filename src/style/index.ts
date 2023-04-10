0140eae1acf099a6b950653d1fdc89a7109d81ac
Fabiano Pires <fabiano.pires>
2023-04-10 10:49:06 -0300

    alpha/profileuser - move documents and create get mechanism

--- src/style/index.ts
+++ src/style/index.ts
@@ -2,7 +2,7 @@ import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import theme from "./theme";
+import theme from "@/style/get-style";
 
 const style = createVuetify({
   theme,
