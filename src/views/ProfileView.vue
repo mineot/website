0368001d140a0c692f9552467ab88bfd0df35ddb
Fabiano Pires <fabiano.pires>
2023-04-14 18:51:06 -0300

    alpha/remake - recreate bio'

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,7 +1,15 @@
 <template>
-  <ProfilePerson></ProfilePerson>
+  <v-row no-gutters>
+    <v-col cols="12" md="5">
+      <ProfilePerson></ProfilePerson>
+    </v-col>
+    <v-col>
+      <ProfilePersonBio></ProfilePersonBio>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup lang="ts">
 import ProfilePerson from "@/components/ProfilePerson.vue";
+import ProfilePersonBio from "@/components/ProfilePersonBio.vue";
 </script>
