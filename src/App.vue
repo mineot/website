03d2029da9a77d8166b5f8ec2e3b5d61e11c7150
Fabiano Pires <fabiano.pires>
2023-04-14 15:22:05 -0300

    alpha/remake - refactory app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer">
-      <SidebarHead></SidebarHead>
-      <SidebarMenu></SidebarMenu>
+
+    <v-navigation-drawer v-model="sideBar">
+
+      <v-img class="mb-2" max-height="80" src="logo.png"></v-img>
+
+      <div v-for="(menu, index) in menus" :key="index" class="mx-2">
+        <v-btn :to="menu.path" color="link" variant="text" block>
+          {{ $t(menu.text) }}
+        </v-btn>
+      </div>
+
     </v-navigation-drawer>
 
     <v-app-bar elevation="0" class="text-text-app">
-      <v-app-bar-nav-icon @click="changeDrawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="changeSidebar"></v-app-bar-nav-icon>
       <v-toolbar-title>{{ $t(infoStore.getTitle) }}</v-toolbar-title>
     </v-app-bar>
 
@@ -15,6 +23,7 @@
         <RouterView />
       </v-container>
     </v-main>
+
   </v-app>
 </template>
 
@@ -24,15 +33,17 @@ import { RouterView } from "vue-router";
 import { useInfoStore } from "@/stores/info";
 import { useDocStore } from "@/stores/doc";
 
-import SidebarHead from "@/components/SidebarHead.vue";
-import SidebarMenu from "@/components/SidebarMenu.vue";
-
 const infoStore = useInfoStore();
 const docStore = useDocStore();
-let drawer = ref(true);
 
-function changeDrawer(): void {
-  drawer.value = !drawer.value;
+const sideBar = ref(true);
+
+const menus: any[] = [
+  { path: "/", text: "profile" }
+];
+
+function changeSidebar(): void {
+  sideBar.value = !sideBar.value;
 }
 
 docStore.recoveryDocument().then();
--- src/components/SidebarHead.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<div>
-  <v-img src="logo.png"></v-img>
-</div>
-</template>
