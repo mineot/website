dfb28959e03b76a9174ac60b72447063396870cc
Fabiano Pires <fabiano.pires>
2023-04-14 17:15:52 -0300

    alpha/remake - rebuild person

--- src/components/ProfilePerson.vue
+++ src/components/ProfilePerson.vue
@@ -2,25 +2,32 @@
   <v-row v-if="docStore.loaded" no-gutters>
 
     <v-col class="d-flex pa-2" cols="12" sm="4">
-      <v-avatar :image="docStore.document.person.photo" class="image-size mx-auto"></v-avatar>
+      <v-avatar :image="person?.photo" class="image-size mx-auto"></v-avatar>
     </v-col>
 
     <v-col class="pa-2">
 
-      <div class="text-h3 mb-2">{{ docStore.document.person.name }}</div>
+      <div class="text-h3 mb-2">{{ person?.name }}</div>
 
-      <div v-for="(contact, index) in contacts" :key="index" class="mb-2">
-        <v-btn color="secondary" variant="tonal" :prepend-icon="contact.icon" :href="contact.href"
-          :target="contact.target">
-          {{ $t(contact.text) }}
-        </v-btn>
-      </div>
+      <v-row>
 
-      <div v-for="(social, index) in socials" :key="index" class="mb-2">
-        <v-btn color="secondary" variant="tonal" :prepend-icon="social.icon" :href="social.src" :target="social.target">
-          {{ social.text }}
-        </v-btn>
-      </div>
+        <v-col cols="12" sm="4">
+          <div v-for="(contact, index) in contacts" :key="index" class="mb-2">
+            <v-btn color="secondary" variant="tonal" :prepend-icon="contact.icon" :href="contact.href"
+              :target="contact.target">
+              {{ $t(contact.text) }}
+            </v-btn>
+          </div>
+        </v-col>
+
+        <v-col>
+          <v-btn v-for="(social, index) in socials" :key="index" class="mr-2 mb-2" color="secondary" variant="tonal"
+            :prepend-icon="social.icon" :href="social.src" :target="social.target">
+            {{ social.text }}
+          </v-btn>
+        </v-col>
+
+      </v-row>
 
     </v-col>
 
@@ -30,49 +37,54 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { useDocStore } from "@/stores/doc";
+import type { Document, Person } from "@/stores/doc/document";
 
 const docStore = useDocStore();
-docStore.loadDocument().then();
+docStore.loadDocument();
+
+const document = computed((): Document | undefined => docStore.loaded ? docStore.document : undefined);
+
+const person = computed((): Person | undefined => document.value ? document.value.person : undefined);
 
 const contacts = computed((): any[] => {
-  if (!docStore.document.person.contact) {
-    return [];
+  if (person?.value?.contact) {
+    const contact: any = person.value.contact;
+    const list: any[] = [];
+
+    if (contact.email) {
+      list.push({ icon: "mdi-email", href: `mailto:${contact.email}`, text: "contacts.email", target: "" });
+    }
+
+    if (contact.phone) {
+      list.push({ icon: "mdi-phone", href: `tel:${contact.phone}`, text: "contacts.phone", target: "" });
+    }
+
+    if (contact.whatsapp) {
+      list.push({ icon: "mdi-cellphone-message", href: contact.whatsapp, text: "contacts.whatsapp", target: "blank" });
+    }
+
+    if (contact.telegram) {
+      list.push({ icon: "mdi-cellphone-message", href: contact.telegram, text: "contacts.telegram", target: "blank" });
+    }
+
+    return list;
   }
 
-  const contact: any = docStore.document.person.contact;
-  const list: any[] = [];
-
-  if (contact.email) {
-    list.push({ icon: "mdi-email", href: `mailto:${contact.email}`, text: "contacts.email", target: "" });
-  }
-
-  if (contact.phone) {
-    list.push({ icon: "mdi-phone", href: `tel:${contact.phone}`, text: "contacts.phone", target: "" });
-  }
-
-  if (contact.whatsapp) {
-    list.push({ icon: "mdi-cellphone-message", href: contact.whatsapp, text: "contacts.whatsapp", target: "blank" });
-  }
-
-  if (contact.telegram) {
-    list.push({ icon: "mdi-cellphone-message", href: contact.telegram, text: "contacts.telegram", target: "blank" });
-  }
-
-  return list;
+  return [];
 });
 
 const socials = computed((): any[] => {
-  if (!docStore.document.person.socials) {
-    return [];
+  if (person?.value?.socials) {
+    const list: any[] = [];
+
+    person.value.socials.forEach((el: any) => {
+      list.push({ icon: "mdi-open-in-new", href: el.src, text: el.name, target: "blank" });
+    });
+
+    return list;
   }
 
-  const list: any[] = [];
-
-  docStore.document.person.socials.forEach((el: any) => {
-    list.push({ icon: "mdi-link", href: el.src, text: el.name, target: "blank" });
-  });
-
-  return list;
+  return [];
 });
 </script>
 
