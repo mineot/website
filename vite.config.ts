04b5de28d9354be048aacce85183edbb0dfa6a49
Mineot <mineot@gmail.com>
2023-08-20 19:56:33 -0300

    alpha/react - alias definitions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@core": path.resolve(__dirname, "src/core"),
+      "@helpers": path.resolve(__dirname, "src/helpers"),
+      "@views": path.resolve(__dirname, "src/views"),
+    },
+  },
+});
