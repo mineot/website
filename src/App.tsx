79e58b9f8c3540579d0235c78f257ca60658cd38
Mineot <mineot@gmail.com>
2023-08-20 22:29:14 -0300

    alpha/react - create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Router } from "@/core/router";
+
 export default function App() {
   return (
     <div>
       <h1>Website: Vite + React + TS</h1>
+      <Router />
     </div>
   );
 }
---
+++ src/views/service-pricing.tsx
@@ -0,0 +1,7 @@
+export function ServicePricing() {
+  return (
+    <div>
+      <h1>Services & Princing</h1>
+    </div>
+  );
+}
