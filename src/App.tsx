ce158ba21c861cdda7fa6b9f5bd5aa649960a462
Mineot <mineot@gmail.com>
2023-08-20 22:39:00 -0300

    alpha/react - add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { Router } from "@/core/router";
+import { useTranslation } from "@/core/i18n";
 
 export default function App() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <h1>Website: Vite + React + TS</h1>
+      <h2>{t("Welcome to React")}</h2>
       <Router />
     </div>
   );
