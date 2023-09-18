9031fdc1dc30822c3ab7b351b6b7f15ecf17cadf
Mineot <mineot@gmail.com>
2023-09-17 21:09:58 -0300

    alpha/react - new sidebar header

--- src/views/components/layout/sidebar.tsx
+++ src/views/components/layout/sidebar.tsx
@@ -4,23 +4,39 @@ export interface Props {
   showClose?: boolean;
 }
 
-export function Sidebar(props: Props) {
-  let buttons: any = "";
-  let fastContactItems: any = [];
-
+function CloseButton(props: Props) {
   if (props.showClose) {
-    buttons = (
-      <button
-        type="button"
-        className="btn btn-link text-secondary float-end"
-        data-bs-dismiss="offcanvas"
-      >
-        <i className="bi bi-x-lg"></i>
-      </button>
+    return (
+      <div className="close">
+        <button
+          type="button"
+          className="btn btn-link text-secondary float-end"
+          data-bs-dismiss="offcanvas"
+        >
+          <i className="bi bi-x-lg"></i>
+        </button>
+      </div>
     );
   }
 
-  fastContactItems = document.fastContacts.map((item: any) => {
+  return <div className="close"></div>;
+}
+
+function Header(props: Props) {
+  return (
+    <div className="header">
+      <div className="header-container">
+        <div className="title">
+          <span>{document.name}</span>
+        </div>
+        <CloseButton showClose={props.showClose} />
+      </div>
+    </div>
+  );
+}
+
+export function Sidebar(props: Props) {
+  const fastContactItems = document.fastContacts.map((item: any) => {
     return (
       <div className="fast-contact-item" key={item.key}>
         <a
@@ -34,27 +50,24 @@ export function Sidebar(props: Props) {
     );
   });
 
+  const body = (
+    <div className="body">
+      <div>
+        <img
+          className="img-fluid img-thumbnail w-75"
+          src={document.photo}
+          alt={document.name}
+        />
+      </div>
+      <div className="mt-2">{document.summary}</div>
+      <div className="fast-contacts">{fastContactItems}</div>
+    </div>
+  );
+
   return (
     <div className="sidebar">
-      <div className="header">
-        <div className="header-container">
-          <div className="title">
-            <span>{document.name}</span>
-          </div>
-          <div className="close">{buttons}</div>
-        </div>
-      </div>
-      <div className="body">
-        <div>
-          <img
-            className="img-fluid img-thumbnail w-75"
-            src={document.photo}
-            alt={document.name}
-          />
-        </div>
-        <div className="mt-2">{document.summary}</div>
-        <div className="fast-contacts">{fastContactItems}</div>
-      </div>
+      <Header showClose={props.showClose} />
+      {body}
     </div>
   );
 }
