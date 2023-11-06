fbd21d85048be577c7c5bb571f18f2236b3912c5
Mineot <mineot@gmail.com>
2023-11-06 00:15:51 -0300

    alpha/react - replace folders

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import { links, routeAnchors } from "./app.components";
 import { TopBar, Sidebar } from "./app.widgets";
+import "./app.scss";
 
 export default function App() {
   return (
