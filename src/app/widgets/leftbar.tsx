e13e38312450a0746f2180ebfc9fcd65be5c771c
Mineot <mineot@gmail.com>
2023-12-04 00:00:40 -0300

    alpha/react - little fixies

--- src/app/widgets/leftbar.tsx
+++ src/app/widgets/leftbar.tsx
@@ -3,16 +3,10 @@ import { useTranslation } from "react-i18next";
 
 export function LeftBar() {
   const { t } = useTranslation();
-  const test = Array(100)
-    .fill("Item")
-    .map((t: string, index: number) => (
-      <div key={index}>{`${t} ${index}`}</div>
-    ));
 
   return (
     <div className="h-100 px-3 py-2">
-      <div>{t("app.title")}</div>
-      {test}
+      <h5>{t("app.title")}</h5>
       <div className="d-none">
         <Menus />
       </div>
