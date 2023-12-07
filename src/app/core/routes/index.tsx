e279299d22f2b1dc1e09602a24da0a00e0864b05
Mineot <mineot@gmail.com>
2023-12-06 23:09:28 -0300

    alpha/card - improvement menus

--- src/app/core/routes/index.tsx
+++ src/app/core/routes/index.tsx
@@ -1,46 +1,16 @@
-import { NavLink, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { elements, Element } from "./elements";
+import { Menus } from "./menus";
 
 export function RoutePoints(): any {
   const { t } = useTranslation();
 
-  return (
-    <Routes>
-      {elements.map((el: Element) => (
-        <Route key={el.uuid} path={el.to} element={t(el.element)} />
-      ))}
-    </Routes>
-  );
+  const routes = elements.map((el: Element) => (
+    <Route key={el.uuid} path={el.to} element={t(el.element)} />
+  ));
+
+  return <Routes>{routes}</Routes>;
 }
 
-export function Menus(): any {
-  const { t } = useTranslation();
-
-  function navClass({ isActive }: any): string {
-    const styles: string[] = ["btn", "btn-link"];
-    if (isActive) {
-      styles.push("text-primary");
-    } else {
-      styles.push("text-secondary");
-    }
-    return styles.join(" ");
-  }
-
-  function iconClass(icon: string): string {
-    return `bi bi-${icon} me-2`;
-  }
-
-  return (
-    <div className="gap-2">
-      {elements.map((el: Element) => (
-        <div key={el.uuid}>
-          <NavLink to={el.to} className={navClass}>
-            <i className={iconClass(el.icon)}></i>
-            <span>{t(el.text)}</span>
-          </NavLink>
-        </div>
-      ))}
-    </div>
-  );
-}
+export { Menus };
---
+++ src/app/core/routes/menus.tsx
@@ -0,0 +1,30 @@
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { elements, Element } from "./elements";
+
+function navClass({ isActive }: any): string {
+  const styles: string[] = ["text-start", "btn"];
+  if (isActive) {
+    styles.push("btn-primary");
+  } else {
+    styles.push("btn-dark");
+  }
+  return styles.join(" ");
+}
+
+function iconClass(icon: string): string {
+  return `bi bi-${icon} me-2`;
+}
+
+export function Menus(): any {
+  const { t } = useTranslation();
+
+  const navs = elements.map((el: Element) => (
+    <NavLink key={el.uuid} to={el.to} className={navClass}>
+      <i className={iconClass(el.icon)}></i>
+      <span>{t(el.text)}</span>
+    </NavLink>
+  ));
+
+  return <div className="d-grid gap-1">{navs}</div>;
+}
