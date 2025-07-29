0c041ac51012f14684ead7e2f94430861c8d881e
Fabiano Pires <fabiano.pires>
2025-07-29 14:03:39 -0300

    :globe_with_meridians: config: add i18next and some locales

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 import "./style.css";
+
+import i18next from "i18next";
+import enUS from "./locales/en-US.json";
+import esAR from "./locales/es-AR.json";
+import ptBR from "./locales/pt-BR.json";
+
+i18next
+  .init({
+    lng: navigator.language,
+    fallbackLng: "en-US",
+    resources: {
+      "pt-BR": { translation: ptBR },
+      "en-US": { translation: enUS },
+      "es-AR": { translation: esAR },
+    },
+  })
+  .then(() => {
+    document.querySelectorAll("[data-i18n]").forEach((el) => {
+      const key = el.getAttribute("data-i18n") || "";
+      el.textContent = i18next.t(key);
+    });
+  });
